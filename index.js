a132f7ba5c7eb11bb2fb668adbb134c2291e0b59
Fikri <fikrizhanking@gmail.com>
2022-06-08 15:21:58 +0700

    Modif ./foo.txt file content (9e59a5c7120b4b1a4ed077ad946588a1)

--- index.js
+++ index.js
@@ -2,28 +2,38 @@ const { exec } = require("child_process");
 const md5 = require('md5');
 const fs = require('fs');
 
-(function() {
-    const randomNumber = Math.floor(Math.random() * 10000000);
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
-    const decision = Math.floor(Math.random() * 2);
+(async function() {
+    while(true) {
+        const randomNumber = Math.floor(Math.random() * 10000000);
 
-    const file = (decision === 1) ? './foo.txt' : './bar.txt';
+        const decision = Math.floor(Math.random() * 2);
 
-    console.log(`Writing on ${file} file`)
-    
-    fs.writeFileSync(file, md5(randomNumber), () => {});
+        const file = (decision === 1) ? './foo.txt' : './bar.txt';
 
-    exec(`git commit -am "Modif ${file} file content (${md5(randomNumber)})"`, (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(stdout);
-    });
+        console.log(`Writing on ${file} file`)
+        
+        fs.writeFileSync(file, md5(randomNumber), () => {});
+
+        exec(`git commit -am "Modif ${file} file content (${md5(randomNumber)})"`, (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(stdout);
+        });
+
+        await sleep(5000)
+    }
 })();
 
 
